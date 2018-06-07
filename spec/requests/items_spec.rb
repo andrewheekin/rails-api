ada1a199567ea0bb2ee846f893fe4225541c203b
Andrew Heekin <andrewheekin@gmail.com>
2018-06-07 10:14:26 -0400

    update jwt and tests

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Items API' do
-  # Initialize the test data
   let(:user) { create(:user) }
   let!(:todo) { create(:todo, created_by: user.id) }
   let!(:items) { create_list(:item, 20, todo_id: todo.id) }
@@ -9,7 +8,6 @@ RSpec.describe 'Items API' do
   let(:id) { items.first.id }
   let(:headers) { valid_headers }
 
-  # Test suite for GET /todos/:todo_id/items
   describe 'GET /todos/:todo_id/items' do
     before { get "/todos/#{todo_id}/items", params: {}, headers: headers }
 
@@ -36,7 +34,6 @@ RSpec.describe 'Items API' do
     end
   end
 
-  # Test suite for GET /todos/:todo_id/items/:id
   describe 'GET /todos/:todo_id/items/:id' do
     before { get "/todos/#{todo_id}/items/#{id}", params: {}, headers: headers }
 
@@ -63,10 +60,8 @@ RSpec.describe 'Items API' do
     end
   end
 
-  # Test suite for PUT /todos/:todo_id/items
   describe 'POST /todos/:todo_id/items' do
     let(:valid_attributes) { { name: 'Visit Narnia', done: false }.to_json }
-
 
     context 'when request attributes are valid' do
       before do
@@ -91,15 +86,12 @@ RSpec.describe 'Items API' do
     end
   end
 
-  # Test suite for PUT /todos/:todo_id/items/:id
   describe 'PUT /todos/:todo_id/items/:id' do
     let(:valid_attributes) { { name: 'Mozart' }.to_json }
 
     before do
       put "/todos/#{todo_id}/items/#{id}", params: valid_attributes, headers: headers
     end
-
-    before { put "/todos/#{todo_id}/items/#{id}", params: valid_attributes }
 
     context 'when item exists' do
       it 'returns status code 204' do
@@ -112,7 +104,7 @@ RSpec.describe 'Items API' do
       end
     end
 
-    context 'when the item does not exist' do
+    context 'when item does not exist' do
       let(:id) { 0 }
 
       it 'returns status code 404' do
@@ -125,7 +117,6 @@ RSpec.describe 'Items API' do
     end
   end
 
-  # Test suite for DELETE /todos/:id
   describe 'DELETE /todos/:id' do
     before { delete "/todos/#{todo_id}/items/#{id}", params: {}, headers: headers }
 
